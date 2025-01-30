112aa716a525c64fbb2cb4b78ff0c77ceae37c1e
Roxane Crépeaux <crepeaux.roxane@gmail.com>
2025-01-30 23:32:01 +0100

    Adding Spring Security configuration

---
+++ back/src/main/java/com/openclassrooms/mddapi/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.openclassrooms.mddapi.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.openclassrooms.mddapi.models.UserModel;
+
+public interface UserRepository extends JpaRepository<UserModel, Integer> {
+	Optional<UserModel> findUserByEmail(String email);
+	Optional<UserModel> findByName(String name);
+}
